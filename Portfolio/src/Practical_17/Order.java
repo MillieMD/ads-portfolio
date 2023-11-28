ccc70de9cf3f6b10625c8362b5b687abd2d698cb
MillieMD <asdapricesavers@gmail.com>
2023-11-28 13:40:37 +0000

    practical 17 order class getters and setters

--- Portfolio/src/Practical_17/Order.java
+++ Portfolio/src/Practical_17/Order.java
@@ -2,13 +2,13 @@ package Practical_17;
 
 public class Order {
 
-    private String id;
+    private final String id;
     private String productName;
     private float unitPrice;
     private int quantity;
     private StatusType status;
 
-    enum StatusType {Pending, Paid, Shipped, Delivered, Refunded};
+    enum StatusType {Pending, Paid, Shipped, Delivered, Refunded}
 
     Order(String id, String name, float price, int quantity){
 
@@ -18,7 +18,44 @@ public class Order {
         this.quantity = quantity;
 
         status = StatusType.Pending;
+    }
 
+    // Setters
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setStatus(StatusType status) {
+        this.status = status;
+    }
+
+    public void setUnitPrice(float unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public StatusType getStatus() {
+        return status;
+    }
+
+    public float getUnitPrice() {
+        return unitPrice;
     }
 
     @Override
